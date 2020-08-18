ea876105f4025c9896ab7844ac304dad158f9f19
Kat Law <kathleenmlaw1@gmail.com>
2020-08-17 22:59:36 -0400

    about overview outline, wip

--- src/components/AboutKat/AboutOverview.tsx
+++ src/components/AboutKat/AboutOverview.tsx
@@ -11,8 +11,18 @@ export const AboutOverview: FC<AboutOverviewProps> = ({ setIndex }) => {
     setIndex(0);
   });
   return (
-    <div className="d-flex justify-content-center">
-      <img className="kat-avatar" src={kat} alt="Kat" />
+    <div className="d-flex flex-row justify-content-center">
+      <div className="d-flex mx-5 my-3 flex-column justify-content-between">
+        <p>Academic Interests</p>
+        <p>Foodies</p>
+      </div>
+      <div className="d-flex justify-content-center align-items-center">
+        <img className="kat-avatar" src={kat} alt="Kat" />
+      </div>
+      <div className="d-flex mx-5 my-3 flex-column justify-content-between">
+        <p>Memes</p>
+        <p>Fun Things</p>
+      </div>
     </div>
   );
 };
