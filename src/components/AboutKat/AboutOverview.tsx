6234390088dcdb2613a41337fa2ca7ea4325e5cb
Kat Law <kathleenmlaw1@gmail.com>
2021-03-12 18:26:34 -0500

    adding snazzy fonts

--- src/components/AboutKat/AboutOverview.tsx
+++ src/components/AboutKat/AboutOverview.tsx
@@ -1,21 +1,19 @@
 import React, { FC } from "react";
-import kat from "../../assets/kat.png";
+import { Button } from "react-bootstrap";
 
 import "../../styles/AboutKat/AboutOverview.css";
 
 export const AboutOverview: FC<unknown> = () => {
   return (
-    <div className="d-flex flex-row justify-content-center">
-      <div className="d-flex mx-5 my-3 flex-column justify-content-between">
-        <p>Learning Interests</p>
-        <p>Foodies</p>
+    <div>
+      <h1 className="aboutMeHeader">About Me</h1>
+      <div className="buttonsRow">
+        <Button className="styledOverviewButton">Learning Interests</Button>
+        <Button className="styledOverviewButton">Books of Note</Button>
       </div>
-      <div className="d-flex justify-content-center align-items-center">
-        <img className="kat-avatar" src={kat} alt="Kat" />
-      </div>
-      <div className="d-flex mx-5 my-3 flex-column justify-content-between">
-        <p>Model-Building</p>
-        <p>Bertha</p>
+      <div className="buttonsRow">
+        <Button className="styledOverviewButton">Model-Building</Button>
+        <Button className="styledOverviewButton">Bertha</Button>
       </div>
     </div>
   );
